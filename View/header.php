aed50ed03dfb26ad1aadafafba8d7f72fc5cb591
alexxstevens <41077253+alexxstevens@users.noreply.github.com>
2020-03-20 17:20:39 -0500

    style phase 1

--- View/header.php
+++ View/header.php
@@ -5,11 +5,14 @@
     <title>To Do List</title>
     <meta name="description" content="To Do List">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../main.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Patrick+Hand|Shadows+Into+Light&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../style/main.css">
   </head>
   <body>
     <div id="page">
       <div id="header">
         <h1>Things to Do</h1>
+        <h5>Manage your life and get productive!</h5>
       </div>
 
